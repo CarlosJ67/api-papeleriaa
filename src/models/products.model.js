6ada7a4f58e99efdd28c57e1431f91728ad29f62
unknown <carlosjesus6384@gmail.com>
2024-02-20 19:16:11 -0600

    first commit

---
+++ src/models/products.model.js
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose";
+
+const productShema= new Schema({
+    barcode: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    description: String,
+    brand: String,
+    price: Number,
+    stock: Number,
+},{
+    versionKey: false, // Para que no se inserte en los documentos "_versionV"
+    timestamps:true
+});
+
+export default model('Products', productShema); // y ocupamos el esquema que vamos a ocupar
